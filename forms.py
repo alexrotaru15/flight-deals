982014e350dab1d6efb28119d76bd8e30d1d7459
alexrot15 <alexrotaru1595@gmail.com>
2021-03-04 22:04:44 +0200

    first save

---
+++ forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=4)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class NewDestinationForm(FlaskForm):
+    destination = StringField('New City')
+    submit = SubmitField('Add')
